ce91f0b0f770852aba2532a1c5dc1de5d60f5e86
emsc0001 <124031667+emsc0001@users.noreply.github.com>
2024-08-26 15:36:55 +0200

    das

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-window.addEventListener("load, start");
+window.addEventListener("load", start);
+
+let secret;
 
 function start () {
     console.log("Kører");
+    secret = randomNumber();
+    document.querySelector("#guesser").addEventListener("submit", recieveInput);
+}   
+
+function randomNumber () {
+    return Math.floor(Math.random()*100)
+}
+                  
+function recieveInput (event) {
+    console.log("Recieve input");
+    event.preventDefault();
+    const form = event.target;
+    const guess = parseInt(form.elements.guess.value, 10);
+    const result = compare(guess, secret);
+
+    if (result === -1) {
+        console.log("Your guess is too low");
+    } else if (result === 1) {
+        console.log("Your guess is too high");
+    } else {
+        console.log("Congratulations! You guessed the secret number");
+    }
+
+
+}
+
+function compare (guess, secret) {
+    if (guess < secret) return -1;
+    if (guess > secret) return 1;
+    if (guess = secret) return 0;
+}
+
+
+
+function outputAnswer(text) {
+    document.querySelector("guesses").insertAdjacentHTML("beforeend", `<li>${text}</li>`)
+    console.log(text);
 }
